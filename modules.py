107b5ae5260087833a571617ba975f70a3945634
masengeshoenock <enockmasengesho1@gmail.com>
2020-03-03 14:45:59 +0200

    first commit

---
+++ modules.py
@@ -0,0 +1,26 @@
+# def greeting(name):
+#     print("hello," + name)
+
+
+
+# person1 = {
+#     "name":"john",
+#     "age":36,
+#     "country":"DR Cong"
+
+# }
+
+
+
+def greeting(name):
+    print("hello," + name)
+
+
+
+person1 = {
+    "name":"masengesho enock",
+    "age":36,
+    "country":"DR Cong"
+    
+}
+
